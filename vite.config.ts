9f17f46eeb656d256f222822ec5725d0d58c855a
tonysoul <tonysoul@foxmail.com>
2022-03-19 15:17:17 +0800

    ci: update deploy base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve:{
-    alias:{
-      '@': path.resolve(__dirname, 'src')
-    }
+  base: '/bootstrap-vue3-vite-ts/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
   css: {
-    preprocessorOptions:{
+    preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/assets/scss/globalVar.scss";`
-      }
-    }
+        additionalData: `@import "@/assets/scss/globalVar.scss";`,
+      },
+    },
   },
-  plugins: [vue()]
-})
+  plugins: [vue()],
+});
